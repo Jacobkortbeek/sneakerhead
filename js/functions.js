e891ca2c5b89277abd9ee4e3414a36109131bd5b
jacobkortbeek <kortbeekdesigns@gmail.com>
2018-12-10 16:36:03 +0000

    home typed

--- js/functions.js
+++ js/functions.js
@@ -63,8 +63,8 @@ console.log($count);
 
 if ( $count > 0 ) {
   for (let i = 0; i < $count ; i++) {
-    var test+i = $(".sentences p").textContent;
-    console.log(test+i);
+    var test[i] = $(".sentences p").textContent;
+    console.log(test[i]);
   }
 }
 

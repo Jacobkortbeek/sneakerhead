d7ddd7cdc83f32bf42d7547525dc487a6392debe
jacobkortbeek <kortbeekdesigns@gmail.com>
2018-12-11 23:21:44 +0000

    home typed

--- front-page.php
+++ front-page.php
@@ -50,12 +50,15 @@
     <div class="two column row">
       <div class="column">
         <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          <?php the_field( 'about_content' ); ?>
         </p>
       </div>
       <div class="column">
         <div class="img">
-          <img src="./assets/img/hermes-rivera-770599-unsplash.jpg" alt="">
+          <?php $about_image = get_field( 'about_image' ); ?>
+          <?php if ( $about_image ) { ?>
+	          <img src="<?php echo $about_image['url']; ?>" alt="<?php echo $about_image['alt']; ?>" />
+          <?php } ?>
         </div>
       </div>
     </div>

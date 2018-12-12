874ab42230264fea46b0d6959c9c852b2a5901ff
jacobkortbeek <kortbeekdesigns@gmail.com>
2018-12-12 22:21:36 +0000

    about typed

--- page-about.php
+++ page-about.php
@@ -13,12 +13,14 @@
     <div class="two column row">
       <div class="column">
         <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          <?php the_field( 'about_text' ); ?>
         </p>
       </div>
       <div class="column">
         <div class="img">
-          <img src="./assets/img/hermes-rivera-770599-unsplash.jpg" alt="">
+          <img src="<?php if ( get_field( 'about_image') ) { ?>
+	<img src="<?php the_field( 'about_image' ); ?>" />
+<?php } ?>" alt="">
         </div>
       </div>
     </div>

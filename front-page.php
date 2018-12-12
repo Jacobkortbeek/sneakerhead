01298590808896f92607ece9e4ad857b02863f35
jacobkortbeek <kortbeekdesigns@gmail.com>
2018-12-12 01:01:02 +0000

    Gallery homepage

--- front-page.php
+++ front-page.php
@@ -119,18 +119,12 @@
       <div class="ui segment">
         <img src="./assets/img/hermes-rivera-770599-unsplash.jpg" alt="" data-focus-left=".30" data-focus-top=".12" data-focus-right=".79" data-focus-bottom=".66" />
       </div>
-      <div class="ui segment">
-        <img src="./assets/img/satria-aditya-782549-unsplash.jpg" alt="" data-focus-left=".30" data-focus-top=".12" data-focus-right=".79" data-focus-bottom=".66" />
-      </div>
     </div>
   <?php elseif ($i<=3) : ?>
     <div class="column computer only tablet only">
       <?php echo $i; ?>
       <div class="ui segment">
         <img src="./assets/img/xavier-teo-469050-unsplash.jpg" alt="" data-focus-left=".30" data-focus-top=".12" data-focus-right=".79" data-focus-bottom=".66" />
-      </div>
-      <div class="ui segment">
-        <img src="./assets/img/malvestida-magazine-458582-unsplash.jpg" alt="" data-focus-left=".30" data-focus-top=".12" data-focus-right=".79" data-focus-bottom=".66" />
       </div>
     </div>
   <?php elseif ($i<=5) : ?>
@@ -139,18 +133,12 @@
       <div class="ui segment">
         <img src="./assets/img/joseph-barrientos-82309-unsplash.jpg" alt="" data-focus-left=".30" data-focus-top=".12" data-focus-right=".79" data-focus-bottom=".66" />
       </div>
-      <div class="ui segment">
-        <img src="./assets/img/nicholas-bui-634691-unsplash.jpg" alt="" data-focus-left=".30" data-focus-top=".12" data-focus-right=".79" data-focus-bottom=".66" />
-      </div>
     </div>
   <?php elseif ($i<=7) : ?>
     <div class="column computer only tablet only">
       <?php echo $i; ?>
       <div class="ui segment">
         <img src="./assets/img/paul-volkmer-522844-unsplash.jpg" alt="" data-focus-left=".30" data-focus-top=".12" data-focus-right=".79" data-focus-bottom=".66" />
-      </div>
-      <div class="ui segment">
-        <img src="./assets/img/greg-tockner-386877-unsplash.jpg" alt="" data-focus-left=".30" data-focus-top=".12" data-focus-right=".79" data-focus-bottom=".66" />
       </div>
     </div>
   <?php endif; ?>

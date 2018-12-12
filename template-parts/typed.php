c65ff5506ee88fece301069cfb554665ee90c3b5
jacobkortbeek <kortbeekdesigns@gmail.com>
2018-12-12 22:08:00 +0000

    Gtyped template part

---
+++ template-parts/typed.php
@@ -0,0 +1,12 @@
+<div class="sixteen wide column">
+  <div class="mainSentences">
+    <p id="main"><?php the_field( 'typed_main_sentence' ); ?></p>
+  </div>
+
+  <div class="sentences">
+    <?php if( have_rows( 'typed_colour_words' ) ) : $i = 0; while ( have_rows( 'typed_colour_words' ) ) : the_row(); ?>
+    <p id="test<?php echo $i; ?>"> <?php the_sub_field( 'coloured_word' ); ?></p>
+  <?php $i++; endwhile; endif; ?>
+  </div>
+  <h2 class="ui huge header" id="typed"> <strong></strong> </h2>
+</div>

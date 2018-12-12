0ae3c4cf577de6c8b00bc5bd3ce111754aa294ee
jacobkortbeek <kortbeekdesigns@gmail.com>
2018-12-12 22:38:10 +0000

    about typed

--- page-about.php
+++ page-about.php
@@ -83,17 +83,19 @@
   <!-- About -->
   <div class="about ui grid stackable container">
     <div class="sixteen wide column">
-      <h2 class="ui huge header"> More About US </h2>
+      <h2 class="ui huge header"><?php the_field( 'extra_info_title' ); ?></h2>
     </div>
     <div class="two column row">
       <div class="column">
         <div class="img">
-          <img src="./assets/img/hermes-rivera-770599-unsplash.jpg" alt="">
+          <?php if ( get_field( 'extra_info_image') ) { ?>
+	<img src="<?php the_field( 'extra_info_image' ); ?>" />
+<?php } ?>
         </div>
       </div>
       <div class="column">
         <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          <?php the_field( 'extra_info_content' ); ?>
         </p>
       </div>
     </div>

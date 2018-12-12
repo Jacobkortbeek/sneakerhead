e0a8e9aa2548fe6d374a94e010bfa80a4b0f21bd
jacobkortbeek <kortbeekdesigns@gmail.com>
2018-12-12 15:50:33 +0000

    Gallery homepage

---
+++ page-about.php
@@ -0,0 +1,186 @@
+<?php get_header(); ?>
+<main class="about">
+  <div class="ui hidden divider"></div>
+  <!-- About -->
+  <div class="about ui grid stackable container">
+    <div class="sixteen wide column">
+      <h2 class="ui huge header" id="typed"> <strong></strong> </h2>
+    </div>
+    <div class="two column row">
+      <div class="column">
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+      </div>
+      <div class="column">
+        <div class="img">
+          <img src="./assets/img/hermes-rivera-770599-unsplash.jpg" alt="">
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="ui hidden divider"></div>
+
+
+  <!-- Turntable -->
+  <div class="turntable-ui ui grid" id="myTurntableTwo">
+    <div class="sixteen wide column">
+      <div class="turntable">
+        <ul>
+          <li data-img-src="./assets/img/sneaker/sneaker-home/img01.jpg"></li>
+          <li data-img-src="./assets/img/sneaker/sneaker-home/img02.jpg"></li>
+          <li data-img-src="./assets/img/sneaker/sneaker-home/img03.jpg"></li>
+          <li data-img-src="./assets/img/sneaker/sneaker-home/img04.jpg"></li>
+          <li data-img-src="./assets/img/sneaker/sneaker-home/img05.jpg"></li>
+          <li data-img-src="./assets/img/sneaker/sneaker-home/img06.jpg"></li>
+          <li data-img-src="./assets/img/sneaker/sneaker-home/img07.jpg"></li>
+          <li data-img-src="./assets/img/sneaker/sneaker-home/img08.jpg"></li>
+          <li data-img-src="./assets/img/sneaker/sneaker-home/img09.jpg"></li>
+          <li data-img-src="./assets/img/sneaker/sneaker-home/img10.jpg"></li>
+          <li data-img-src="./assets/img/sneaker/sneaker-home/img11.jpg"></li>
+          <li data-img-src="./assets/img/sneaker/sneaker-home/img12.jpg"></li>
+          <li data-img-src="./assets/img/sneaker/sneaker-home/img13.jpg"></li>
+          <li data-img-src="./assets/img/sneaker/sneaker-home/img14.jpg"></li>
+          <li data-img-src="./assets/img/sneaker/sneaker-home/img15.jpg"></li>
+          <li data-img-src="./assets/img/sneaker/sneaker-home/img16.jpg"></li>
+          <li data-img-src="./assets/img/sneaker/sneaker-home/img17.jpg"></li>
+          <li data-img-src="./assets/img/sneaker/sneaker-home/img18.jpg"></li>
+          <li data-img-src="./assets/img/sneaker/sneaker-home/img19.jpg"></li>
+          <li data-img-src="./assets/img/sneaker/sneaker-home/img20.jpg"></li>
+          <li data-img-src="./assets/img/sneaker/sneaker-home/img21.jpg"></li>
+          <li data-img-src="./assets/img/sneaker/sneaker-home/img22.jpg"></li>
+          <li data-img-src="./assets/img/sneaker/sneaker-home/img23.jpg"></li>
+          <li data-img-src="./assets/img/sneaker/sneaker-home/img24.jpg"></li>
+          <li data-img-src="./assets/img/sneaker/sneaker-home/img25.jpg"></li>
+          <li data-img-src="./assets/img/sneaker/sneaker-home/img26.jpg"></li>
+          <li data-img-src="./assets/img/sneaker/sneaker-home/img27.jpg"></li>
+          <li data-img-src="./assets/img/sneaker/sneaker-home/img28.jpg"></li>
+          <li data-img-src="./assets/img/sneaker/sneaker-home/img29.jpg"></li>
+          <li data-img-src="./assets/img/sneaker/sneaker-home/img30.jpg"></li>
+          <li data-img-src="./assets/img/sneaker/sneaker-home/img31.jpg"></li>
+          <li data-img-src="./assets/img/sneaker/sneaker-home/img32.jpg"></li>
+          <li data-img-src="./assets/img/sneaker/sneaker-home/img33.jpg"></li>
+          <li data-img-src="./assets/img/sneaker/sneaker-home/img34.jpg"></li>
+          <li data-img-src="./assets/img/sneaker/sneaker-home/img36.jpg"></li>
+        </ul>
+      </div>
+    </div>
+  </div>
+
+  <div class="turntable-ui ui grid" id="myTurntable">
+    <div class="sixteen wide column">
+      <div class="turntable">
+        <ul>
+          <li data-img-src="./assets/img/sneaker/sneaker-home/img01.jpg"></li>
+          <li data-img-src="./assets/img/sneaker/sneaker-home/img02.jpg"></li>
+          <li data-img-src="./assets/img/sneaker/sneaker-home/img03.jpg"></li>
+          <li data-img-src="./assets/img/sneaker/sneaker-home/img04.jpg"></li>
+          <li data-img-src="./assets/img/sneaker/sneaker-home/img05.jpg"></li>
+          <li data-img-src="./assets/img/sneaker/sneaker-home/img06.jpg"></li>
+          <li data-img-src="./assets/img/sneaker/sneaker-home/img07.jpg"></li>
+          <li data-img-src="./assets/img/sneaker/sneaker-home/img08.jpg"></li>
+          <li data-img-src="./assets/img/sneaker/sneaker-home/img09.jpg"></li>
+          <li data-img-src="./assets/img/sneaker/sneaker-home/img10.jpg"></li>
+          <li data-img-src="./assets/img/sneaker/sneaker-home/img11.jpg"></li>
+          <li data-img-src="./assets/img/sneaker/sneaker-home/img12.jpg"></li>
+          <li data-img-src="./assets/img/sneaker/sneaker-home/img13.jpg"></li>
+          <li data-img-src="./assets/img/sneaker/sneaker-home/img14.jpg"></li>
+          <li data-img-src="./assets/img/sneaker/sneaker-home/img15.jpg"></li>
+          <li data-img-src="./assets/img/sneaker/sneaker-home/img16.jpg"></li>
+          <li data-img-src="./assets/img/sneaker/sneaker-home/img17.jpg"></li>
+          <li data-img-src="./assets/img/sneaker/sneaker-home/img18.jpg"></li>
+          <li data-img-src="./assets/img/sneaker/sneaker-home/img19.jpg"></li>
+          <li data-img-src="./assets/img/sneaker/sneaker-home/img20.jpg"></li>
+          <li data-img-src="./assets/img/sneaker/sneaker-home/img21.jpg"></li>
+          <li data-img-src="./assets/img/sneaker/sneaker-home/img22.jpg"></li>
+          <li data-img-src="./assets/img/sneaker/sneaker-home/img23.jpg"></li>
+          <li data-img-src="./assets/img/sneaker/sneaker-home/img24.jpg"></li>
+          <li data-img-src="./assets/img/sneaker/sneaker-home/img25.jpg"></li>
+          <li data-img-src="./assets/img/sneaker/sneaker-home/img26.jpg"></li>
+          <li data-img-src="./assets/img/sneaker/sneaker-home/img27.jpg"></li>
+          <li data-img-src="./assets/img/sneaker/sneaker-home/img28.jpg"></li>
+          <li data-img-src="./assets/img/sneaker/sneaker-home/img29.jpg"></li>
+          <li data-img-src="./assets/img/sneaker/sneaker-home/img30.jpg"></li>
+          <li data-img-src="./assets/img/sneaker/sneaker-home/img31.jpg"></li>
+          <li data-img-src="./assets/img/sneaker/sneaker-home/img32.jpg"></li>
+          <li data-img-src="./assets/img/sneaker/sneaker-home/img33.jpg"></li>
+          <li data-img-src="./assets/img/sneaker/sneaker-home/img34.jpg"></li>
+          <li data-img-src="./assets/img/sneaker/sneaker-home/img36.jpg"></li>
+        </ul>
+      </div>
+    </div>
+  </div>
+
+  <div class="turntable-ui ui grid" id="myTurntableThree">
+    <div class="sixteen wide column">
+      <div class="turntable">
+        <ul>
+          <li data-img-src="./assets/img/sneaker/sneaker-home/img01.jpg"></li>
+          <li data-img-src="./assets/img/sneaker/sneaker-home/img02.jpg"></li>
+          <li data-img-src="./assets/img/sneaker/sneaker-home/img03.jpg"></li>
+          <li data-img-src="./assets/img/sneaker/sneaker-home/img04.jpg"></li>
+          <li data-img-src="./assets/img/sneaker/sneaker-home/img05.jpg"></li>
+          <li data-img-src="./assets/img/sneaker/sneaker-home/img06.jpg"></li>
+          <li data-img-src="./assets/img/sneaker/sneaker-home/img07.jpg"></li>
+          <li data-img-src="./assets/img/sneaker/sneaker-home/img08.jpg"></li>
+          <li data-img-src="./assets/img/sneaker/sneaker-home/img09.jpg"></li>
+          <li data-img-src="./assets/img/sneaker/sneaker-home/img10.jpg"></li>
+          <li data-img-src="./assets/img/sneaker/sneaker-home/img11.jpg"></li>
+          <li data-img-src="./assets/img/sneaker/sneaker-home/img12.jpg"></li>
+          <li data-img-src="./assets/img/sneaker/sneaker-home/img13.jpg"></li>
+          <li data-img-src="./assets/img/sneaker/sneaker-home/img14.jpg"></li>
+          <li data-img-src="./assets/img/sneaker/sneaker-home/img15.jpg"></li>
+          <li data-img-src="./assets/img/sneaker/sneaker-home/img16.jpg"></li>
+          <li data-img-src="./assets/img/sneaker/sneaker-home/img17.jpg"></li>
+          <li data-img-src="./assets/img/sneaker/sneaker-home/img18.jpg"></li>
+          <li data-img-src="./assets/img/sneaker/sneaker-home/img19.jpg"></li>
+          <li data-img-src="./assets/img/sneaker/sneaker-home/img20.jpg"></li>
+          <li data-img-src="./assets/img/sneaker/sneaker-home/img21.jpg"></li>
+          <li data-img-src="./assets/img/sneaker/sneaker-home/img22.jpg"></li>
+          <li data-img-src="./assets/img/sneaker/sneaker-home/img23.jpg"></li>
+          <li data-img-src="./assets/img/sneaker/sneaker-home/img24.jpg"></li>
+          <li data-img-src="./assets/img/sneaker/sneaker-home/img25.jpg"></li>
+          <li data-img-src="./assets/img/sneaker/sneaker-home/img26.jpg"></li>
+          <li data-img-src="./assets/img/sneaker/sneaker-home/img27.jpg"></li>
+          <li data-img-src="./assets/img/sneaker/sneaker-home/img28.jpg"></li>
+          <li data-img-src="./assets/img/sneaker/sneaker-home/img29.jpg"></li>
+          <li data-img-src="./assets/img/sneaker/sneaker-home/img30.jpg"></li>
+          <li data-img-src="./assets/img/sneaker/sneaker-home/img31.jpg"></li>
+          <li data-img-src="./assets/img/sneaker/sneaker-home/img32.jpg"></li>
+          <li data-img-src="./assets/img/sneaker/sneaker-home/img33.jpg"></li>
+          <li data-img-src="./assets/img/sneaker/sneaker-home/img34.jpg"></li>
+          <li data-img-src="./assets/img/sneaker/sneaker-home/img36.jpg"></li>
+        </ul>
+      </div>
+    </div>
+  </div>
+  <div class="ui hidden divider"></div>
+
+  <!-- About -->
+  <div class="about ui grid stackable container">
+    <div class="sixteen wide column">
+      <h2 class="ui huge header"> More About US </h2>
+    </div>
+    <div class="two column row">
+      <div class="column">
+        <div class="img">
+          <img src="./assets/img/hermes-rivera-770599-unsplash.jpg" alt="">
+        </div>
+      </div>
+      <div class="column">
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+      </div>
+    </div>
+  </div>
+
+  <div class="ui hidden divider"></div>
+
+
+
+
+
+</main>
+<?php get_footer(); ?>

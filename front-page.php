ac5d6d1cee2043d962e25936e01f6d2c7f705d5d
jacobkortbeek <kortbeekdesigns@gmail.com>
2018-12-12 01:42:35 +0000

    Gallery homepage

--- front-page.php
+++ front-page.php
@@ -113,7 +113,7 @@
 <?php if( have_rows('home_gallery') ): ?>
   <div class="ui four column stackable padded equal height grid gallery">
     <?php $i=0; while ( have_rows('home_gallery') ) : the_row(); ?>
-        <?php if($i>=1) : ?>
+        <?php if($i<1) : ?>
           <?php if($i===0) : ?>
             <div class="column">
           <?php endif; ?>
@@ -124,7 +124,7 @@
         <?php if($i===1) : ?>
           </div>
         <?php endif; ?>
-        <?php elseif ($i<=3) : ?>
+        <?php elseif ($i<3) : ?>
           <?php if ($i===2) : ?>
             <div class="column computer only tablet only">
           <?php endif ?>
@@ -135,7 +135,7 @@
           <?php if($i===3) : ?>
             </div>
           <?php endif; ?>
-        <?php elseif ($i<=5) : ?>
+        <?php elseif ($i<5) : ?>
           <?php if ($i===4) : ?>
             <div class="column computer only tablet only">
           <?php endif; ?>
@@ -146,7 +146,7 @@
           <?php if($i===5) : ?>
             </div>
           <?php endif; ?>
-        <?php elseif ($i<=7) : ?>
+        <?php elseif ($i<7) : ?>
           <?php if ($i===6) : ?>
             <div class="column computer only tablet only">
           <?php endif; ?>

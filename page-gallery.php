a1dea27af6bf8746ce039bf12758da82ea2d6c1e
jacobkortbeek <kortbeekdesigns@gmail.com>
2018-12-13 01:14:32 +0000

    gallery page template

--- page-gallery.php
+++ page-gallery.php
@@ -13,14 +13,12 @@
   </div>
 
   <div class="ui hidden divider"></div>
-<?php $galArray = Array(); ?>
 <?php $iNum=0; if( have_rows('gallery_images') ): while ( have_rows('gallery_images') ) : the_row(); ?>
-
 <?php $iNum++; endwhile; endif; ?>
 <?php  echo $iNum; $iNumFourth = $iNum / 4; $iNumRound =  round($iNumFourth); echo $iNumFourth; echo$iNumRound; ?>
+
   <div class="ui four column stackable padded equal height grid gallery">
     <?php if( have_rows('gallery_images') ): ?>
-      <?php echo sizeof('gallery_images'); ?>
       <?php $i=0; while ( have_rows('gallery_images') ) : the_row(); ?>
         <?php if ($i===0) : ?>
     <div class="column">
@@ -29,12 +27,12 @@
       <div class="ui segment">
         <img src="<?php the_sub_field( 'gallery_image' ) ?>" alt="" data-focus-left=".30" data-focus-top=".12" data-focus-right=".79" data-focus-bottom=".66" />
       </div>
-      <?php if ($i===$iNumRound) : ?>
+      <?php if ($i===12) : ?>
     </div>
     <?php endif; ?>
     <?php if($i===0) :
       $i++;
-     elseif ($i===$iNumRound) :
+     elseif ($i===12) :
        $i=0;
      endif;?>
   <?php endwhile; ?>

b10e70ef251db37ab8e08301970a9c4f55fa08f1
jacobkortbeek <kortbeekdesigns@gmail.com>
2018-12-13 00:33:49 +0000

    gallery page template

--- page-gallery.php
+++ page-gallery.php
@@ -5,5 +5,53 @@
  */
 ?>
 <?php get_header(); ?>
+<main class="gallery">
+  <div class="heading ui center aligned segment" style="background-image: linear-gradient( rgba(0, 0, 0, 0.25), rgba(0, 0, 0, 0.25) ), url('./assets/img/nicholas-bui-634691-unsplash.jpg')!important;">
+    <div class="ui text container">
+      <h1><?php the_title(); ?></h1>
+    </div>
+  </div>
 
+  <div class="ui hidden divider"></div>
+
+  <div class="ui four column stackable padded equal height grid gallery">
+    <div class="column">
+      <div class="ui segment">
+        <img src="./assets/img/hermes-rivera-770599-unsplash.jpg" alt="" data-focus-left=".30" data-focus-top=".12" data-focus-right=".79" data-focus-bottom=".66" />
+      </div>
+      <div class="ui segment">
+        <img src="./assets/img/satria-aditya-782549-unsplash.jpg" alt="" data-focus-left=".30" data-focus-top=".12" data-focus-right=".79" data-focus-bottom=".66" />
+      </div>
+    </div>
+    <div class="column">
+      <div class="ui segment">
+        <img src="./assets/img/xavier-teo-469050-unsplash.jpg" alt="" data-focus-left=".30" data-focus-top=".12" data-focus-right=".79" data-focus-bottom=".66" />
+      </div>
+      <div class="ui segment">
+        <img src="./assets/img/malvestida-magazine-458582-unsplash.jpg" alt="" data-focus-left=".30" data-focus-top=".12" data-focus-right=".79" data-focus-bottom=".66" />
+      </div>
+    </div>
+    <div class="column">
+      <div class="ui segment">
+        <img src="./assets/img/joseph-barrientos-82309-unsplash.jpg" alt="" data-focus-left=".30" data-focus-top=".12" data-focus-right=".79" data-focus-bottom=".66" />
+      </div>
+      <div class="ui segment">
+        <img src="./assets/img/nicholas-bui-634691-unsplash.jpg" alt="" data-focus-left=".30" data-focus-top=".12" data-focus-right=".79" data-focus-bottom=".66" />
+      </div>
+    </div>
+    <div class="column">
+      <div class="ui segment">
+        <img src="./assets/img/paul-volkmer-522844-unsplash.jpg" alt="" data-focus-left=".30" data-focus-top=".12" data-focus-right=".79" data-focus-bottom=".66" />
+      </div>
+      <div class="ui segment">
+        <img src="./assets/img/greg-tockner-386877-unsplash.jpg" alt="" data-focus-left=".30" data-focus-top=".12" data-focus-right=".79" data-focus-bottom=".66" />
+      </div>
+    </div>
+  </div>
+
+
+
+
+
+</main>
 <?php get_footer(); ?>

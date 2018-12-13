7d72d23bd3282790f95e7fb5c35c5d41b115681c
jacobkortbeek <kortbeekdesigns@gmail.com>
2018-12-13 02:31:50 +0000

    gallery page template

--- functions.php
+++ functions.php
@@ -23,7 +23,7 @@ add_action( 'init', 'create_posttype' );
 
   function wpt_theme_styles() {
     wp_enqueue_style( 'font', 'https://fonts.googleapis.com/css?family=KoHo' );
-    wp_enqueue_style( 'semantic_ui', get_template_directory_uri() . '/css/semantic.min.css' );
+    wp_enqueue_style( 'semantic_ui', get_template_directory_uri() . '/css/semantic/semantic.min.css' );
     wp_enqueue_style( 'direction_reveal', get_template_directory_uri() . '/css/direction-reveal.css' );
     wp_enqueue_style( 'turn_table', get_template_directory_uri() . '/css/turntable.css' );
     wp_enqueue_style( 'main_css', get_template_directory_uri() . '/style.css' );
@@ -31,7 +31,7 @@ add_action( 'init', 'create_posttype' );
   add_action( 'wp_enqueue_scripts', 'wpt_theme_styles' );
 
   function wpt_theme_js() {
-    wp_enqueue_script( 'semantic_js', get_template_directory_uri() . '/js/semantic.js', array('jquery'), ' ', true );
+    wp_enqueue_script( 'semantic_js', get_template_directory_uri() . '/css/semantic/semantic.min.js', array('jquery'), ' ', true );
     wp_enqueue_script( 'turntable_js', get_template_directory_uri() . '/js/turntable.js', array('jquery'), ' ', true );
     wp_enqueue_script( 'typed_js', get_template_directory_uri() . '/js/typed.js', array('jquery'), ' ', true );
     wp_enqueue_script( 'responsify_js', get_template_directory_uri() . '/js/responsify.js', array('jquery'), ' ', true );

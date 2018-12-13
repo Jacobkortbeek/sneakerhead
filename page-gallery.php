811cda92ff63aa353871312d0b0737b647952c36
jacobkortbeek <kortbeekdesigns@gmail.com>
2018-12-13 01:08:13 +0000

    gallery page template

--- page-gallery.php
+++ page-gallery.php
@@ -17,7 +17,7 @@
 <?php $iNum=0; if( have_rows('gallery_images') ): while ( have_rows('gallery_images') ) : the_row(); ?>
 
 <?php $iNum++; endwhile; endif; ?>
-<?php  echo $iNum; $iNumFourth = $iNum / 4; round($iNumFourth); echo $iNumFourth; echo(round(11.47)); ?>
+<?php  echo $iNum; $iNumFourth = $iNum / 4; round($iNumFourth); echo $iNumFourth; echo(round(11.75)); ?>
   <div class="ui four column stackable padded equal height grid gallery">
     <?php if( have_rows('gallery_images') ): ?>
       <?php echo sizeof('gallery_images'); ?>

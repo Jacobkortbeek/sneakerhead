cc725af0d8216db6b4efd53748b575b2e857139b
jacobkortbeek <kortbeekdesigns@gmail.com>
2018-12-13 23:17:27 +0000

    footer widgets

--- footer.php
+++ footer.php
@@ -6,13 +6,19 @@
       <div class="content">
         <h2 class="header">Title</h2>
           <?php if( !dynamic_sidebar( 'footer-text' )) : ?>
+            no sidebar text
+          <?php endif; ?>
       </div>
     </div>
     <div class="four wide column very padded">
       <?php if( !dynamic_sidebar( 'menu-one' )) : ?>
+        no sidebar text
+      <?php endif; ?>
     </div>
     <div class="four wide column very padded">
       <?php if( !dynamic_sidebar( 'menu-two' )) : ?>
+        no sidebar text
+      <?php endif; ?>
     </div>
   </div>
 </div>

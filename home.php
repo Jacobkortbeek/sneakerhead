1c4a8160d0dbea553338a0f7c05bde168929623a
jacobkortbeek <kortbeekdesigns@gmail.com>
2018-12-13 02:33:51 +0000

    gallery page template

--- home.php
+++ home.php
@@ -29,7 +29,7 @@
           <div class="ui two column stackable row">
             <div class="column">
               <h2><?php the_title(); ?></h2>
-              
+
                 <?php the_excerpt(); ?>
 
             </div>
@@ -51,6 +51,56 @@
   </a>
 <div class="ui hidden divider small"></div>
 <?php endwhile; endif; ?>
+<a href="#">
+  <div class="ui segment blog">
+    <div class="ui grid">
+      <div class="ui two column stackable row">
+        <div class="column">
+          <h2>Title</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+        </div>
+        <div class="column">
+          <div class="ui segment img">
+            <img src="./assets/img/malvestida-magazine-458582-unsplash.jpg" alt="" data-focus-left=".30" data-focus-top=".12" data-focus-right=".79" data-focus-bottom=".66" />
+          </div>
+        </div>
+      </div>
+      <div class="sixteen wide column">
+        <button class="large ui right floated button">
+          More
+        </button>
+      </div>
+  </div>
+</div>
+</a>
+<div class="ui hidden divider small"></div>
+<a href="#">
+<div class="ui segment blog">
+  <div class="ui grid">
+    <div class="ui two column stackable row">
+      <div class="column">
+        <h2>Title</h2>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+      </div>
+      <div class="column">
+        <div class="ui segment img">
+          <img src="./assets/img/malvestida-magazine-458582-unsplash.jpg" alt="" data-focus-left=".30" data-focus-top=".12" data-focus-right=".79" data-focus-bottom=".66" />
+        </div>
+      </div>
+    </div>
+    <div class="sixteen wide column">
+      <button class="large ui right floated button">
+        More
+      </button>
+    </div>
+</div>
+</div>
+</a>
+<div class="ui hidden divider small"></div>
 </div>
 </main>
 <?php get_footer(); ?>

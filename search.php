a9a1a6448749fca929324bba6f31686b5ccb5ca2
jacobkortbeek <kortbeekdesigns@gmail.com>
2018-12-15 22:10:49 +0000

    search

--- search.php
+++ search.php
@@ -6,13 +6,12 @@ Template Name: Search Page
 <?php get_header(); ?>
 <main class="fourOfour">
   <div class="wrap">
-  	<div id="primary" class="content-area">
-  		<main id="main" class="site-main" role="main">
+
 
   			<?php get_search_form(); ?>
 
-  		</main><!-- #main -->
-  	</div><!-- #primary -->
+
+
   </div><!-- .wrap -->
 
   </main>

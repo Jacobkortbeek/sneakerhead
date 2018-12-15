41da5f44b9a51eaf7a980cca4bb853bd94444ab7
jacobkortbeek <kortbeekdesigns@gmail.com>
2018-12-15 21:56:08 +0000

    404

---
+++ 404.php
@@ -0,0 +1,12 @@
+<main class="fourOfour">
+
+  <div class="ui container">
+    <div class="row">
+      <div class="sixteen wide column">
+        <h1 class="header">404</h1>
+        <h2>Someone is getting fired</h2>
+      </div>
+    </div>
+  </div>
+
+  </main>

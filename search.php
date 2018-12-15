4a86ae48c294e13acb5087fd986c8dc87c6feba7
jacobkortbeek <kortbeekdesigns@gmail.com>
2018-12-15 22:07:31 +0000

    search

--- search.php
+++ search.php
@@ -4,5 +4,16 @@ Template Name: Search Page
 */
 ?>
 <?php get_header(); ?>
-<?php get_search_form(); ?>
+<main class="fourOfour">
+
+  <div class="ui fluid category search">
+<div class="ui icon input">
+  <?php get_search_form(); ?>
+  <i class="search icon"></i>
+</div>
+<div class="results"></div>
+</div>
+
+  </main>
+
 <?php get_footer(); ?>

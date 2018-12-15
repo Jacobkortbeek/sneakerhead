964782bf5405346a7336d8ab62e2c2e7feffeca7
jacobkortbeek <kortbeekdesigns@gmail.com>
2018-12-15 22:12:24 +0000

    search

--- search.php
+++ search.php
@@ -10,7 +10,14 @@ Template Name: Search Page
 
   			<?php get_search_form(); ?>
 
+        <?php
 
+        global $query_string;
+
+        wp_parse_str( $query_string, $search_query );
+        $search = new WP_Query( $search_query );
+
+        ?>
 
   </div><!-- .wrap -->
 
